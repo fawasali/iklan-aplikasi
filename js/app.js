fc03397c98591ff98e6b0b6b4c697ac2a8b8b48a
ali <68717503+fawasali@users.noreply.github.com>
2023-04-15 02:28:12 +0700

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+        $('.wrapper-widget').carousel({
+    
+        // the number of images to display
+        num: 3, 
+    
+        // max width of the active image
+        maxWidth: 320,
+    
+        // min width of the active image
+        maxHeight: 609, 
+    
+      // enable auto play
+      autoPlay: false,
+    
+      // autoplay interval
+      showTime: 1000,
+    
+      // animation speed
+      animationTime: 300,
+    
+        // 0.0 - 1.0
+        scale: 0.9,
+    
+        // the distance between images
+        distance: 200
+    
+        });
